d57a81d5d49d0ca2db4b6558d3a00bb03afe079c
Gabriela Fragiadakis <gabrielafragiadakis@Gabrielas-MacBook-Pro.local>
2019-12-05 09:29:05 -0800

    added bad coding examples

---
+++ bad-code-example/mycrappyCode_FINAL-v3.R
@@ -0,0 +1,7 @@
+
+# here's some code I wrote today
+my_variable <- runif(10, min = 0, max = 1000)
+my_other_variable <- runif(10, min = 50, max = 800)
+my_changed_variable <- asinh(my_variable/5)
+my_changed_other_variable <- asinh(my_changed_variable/5)
+
